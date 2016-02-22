77aa4414f9bc45ca0c4ca686a52edaa6bccce4ff
Ammar Bahman <bo3mrh@gmail.com>
2016-02-22 17:16:56 -0500

    add percentage air velocity

--- ComponentTests/temp_velocity measurment/velocity_plot_v2.py
+++ ComponentTests/temp_velocity measurment/velocity_plot_v2.py
@@ -203,26 +203,38 @@ for i in range(len(Test)):
     percentage = lang_res_cir_sum / lang_res_100_sum #notice that the sum of this array =1.0 (100%)
     print "Air velocity percentage = ", percentage
     print ' '
-
-
-        
-    #plt.figure()
+    
+    ##########plot air velocity percentages##########
     ax = plt.subplot(1, 3, i+1)
-    plt.plot(average[i],y,'bo',label=r'Data')
-    plt.plot(vg,grid_y,'r',label=r'Cubic polynomial')
-    plt.plot(lang_res,grid_y,'g--',label=r'Lagrange polynomial')
-    plt.ylim(0,22.5)
-    plt.xlim(0,6)
-    plt.yticks([0, 4, 8, 12, 16, 20, 22.5],
-               [r'$0$', r'$4$', r'$8$', r'$12$',r'$16$', r'$20$', r'$22.5$'])
-    plt.xlabel('Velocity [m/s]')
-    plt.ylabel('Evaporator height [in]')
-    plt.title('Velocity fit of Test '+Test[i])
+    plt.bar(np.arange(1,7,1)-0.4,percentage*100,label=r'velocity percentage')
+    plt.ylim(0,25)
+    plt.xlim(0,7)
+    plt.xticks([0, 1, 2, 3, 4, 5, 6, 7],
+               [r'$top$', r'$1$', r'$2$', r'$3$',r'$4$', r'$5$', r'$6$', r'$bottom$'])
+    plt.xlabel('Circuit number')
+    plt.ylabel('Percentage [\%]')
+    plt.title('Air Velocity \% of Test '+Test[i])
+    
+    
+    #########plot air velocity fit#############    
+    #plt.figure()
+#     ax = plt.subplot(1, 3, i+1)
+#     plt.plot(average[i],y,'bo',label=r'Data')
+#     plt.plot(vg,grid_y,'r',label=r'Cubic polynomial')
+#     plt.plot(lang_res,grid_y,'g--',label=r'Lagrange polynomial')
+#     plt.ylim(0,22.5)
+#     plt.xlim(0,6)
+#     plt.yticks([0, 4, 8, 12, 16, 20, 22.5],
+#                [r'$0$', r'$4$', r'$8$', r'$12$',r'$16$', r'$20$', r'$22.5$'])
+#     plt.xlabel('Velocity [m/s]')
+#     plt.ylabel('Evaporator height [in]')
+#     plt.title('Velocity fit of Test '+Test[i])
 #     plt.legend(loc='best',fancybox=False)
 #     plt.savefig('velocity_profile_v2/velocity_curve_test'+Test[i]+'.pdf')
 #     plt.show()
 fig.set_tight_layout(True)
-leg = ax.legend(bbox_to_anchor=(-2.54, 0.03), loc='lower left', borderaxespad=0.)
-leg.get_frame().set_alpha(0.7)
+#leg = ax.legend(bbox_to_anchor=(-2.54, 0.03), loc='lower left', borderaxespad=0.)
+#leg.get_frame().set_alpha(0.7)
 #plt.savefig('velocity_profile_v2/velocity_curve_combined.pdf')
-#plt.show()
+plt.savefig('velocity_profile_v2/velocity_percent_combined.pdf')
+plt.show()
